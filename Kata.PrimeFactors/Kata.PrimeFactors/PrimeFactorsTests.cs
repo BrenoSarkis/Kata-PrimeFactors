4bee5af535bd5281230dc09be6452a3b53aafcaa
Breno Sarkis <sarkisbreno@gmail.com>
2018-07-25 17:54:48 +0200

    Recreates this repo by using code from the Kata one

---
+++ Kata.PrimeFactors/Kata.PrimeFactors/PrimeFactorsTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Kata.PrimeFactors
+{
+    [TestFixture]
+    public class PrimeFactorsTests
+    {
+        private IEnumerable<int> ListOf(params int[] n)
+        {
+            return n;
+        }
+
+        [Test]
+        public void Factors()
+        {
+            Assert.That(PrimeFactorsCalculator.PrimeFactorsOf(1), Is.Empty);
+            Assert.That(PrimeFactorsCalculator.PrimeFactorsOf(2), Is.EqualTo(ListOf(2)));
+            Assert.That(PrimeFactorsCalculator.PrimeFactorsOf(3), Is.EqualTo(ListOf(3)));
+            Assert.That(PrimeFactorsCalculator.PrimeFactorsOf(4), Is.EqualTo(ListOf(2, 2)));
+            Assert.That(PrimeFactorsCalculator.PrimeFactorsOf(5), Is.EqualTo(ListOf(5)));
+            Assert.That(PrimeFactorsCalculator.PrimeFactorsOf(6), Is.EqualTo(ListOf(2, 3)));
+            Assert.That(PrimeFactorsCalculator.PrimeFactorsOf(7), Is.EqualTo(ListOf(7)));
+            Assert.That(PrimeFactorsCalculator.PrimeFactorsOf(8), Is.EqualTo(ListOf(2, 2, 2)));
+            Assert.That(PrimeFactorsCalculator.PrimeFactorsOf(9), Is.EqualTo(ListOf(3, 3)));
+        }
+    }
+
+    public class PrimeFactorsCalculator
+    {
+        public static List<int> PrimeFactorsOf(int n)
+        {
+            var factors = new List<int>();
+
+            for (int divisor = 2; n > 1; divisor++)
+            {
+                for (; n % divisor == 0; n /= divisor)
+                {
+                    factors.Add(divisor);
+                }
+            }
+
+            return factors;
+        }
+    }
+}
